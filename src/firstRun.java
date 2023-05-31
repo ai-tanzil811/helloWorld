cb8ec6cebf3264df250c896fc4c6f5a7371ffbd1
ai-tanzil811 <aitanzil2230028@bscse.uiu.ac.bd>
2023-05-31 20:31:57 +0600

    java

---
+++ src/firstRun.java
@@ -0,0 +1,18 @@
+public class firstRun {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        secondRun.main(args);
+        thirdRun.main(args);
+
+    }
+}
+class secondRun {
+    public static void main(String[] args) {
+        System.out.println("AShraful ISlam TAnzil");
+    }
+}
+ class thirdRun {
+    public static void main(String[] args) {
+        System.out.println("Ashraful ISlam Tanzil World!");
+    }
+}
